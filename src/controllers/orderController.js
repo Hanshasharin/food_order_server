5442fff18a92bc3e30aef89e5a444742a8b88b1e
unknown <sharinhansha@gmail.com>
2025-09-21 22:36:27 +0530

    vercel

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -63,8 +63,8 @@ const orderFoodController = async (req, res) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url: `https://vercel.com/hansha-sharins-projects/food-order-client/order-success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `https://vercel.com/hansha-sharins-projects/food-order-client/order-cancel`,
+      success_url: `https://food-order-client-1ikuhou9e-hansha-sharins-projects.vercel.app/order-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://food-order-client-1ikuhou9e-hansha-sharins-projects.vercel.app/order-cancel`,
     });
 
     // Save pending order
